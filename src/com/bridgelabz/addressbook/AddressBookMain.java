e28435f871dad0ef220632f457fd323958b06774
PREETI2096 <preetirg96@gmail.com>
2022-11-17 11:32:29 +0530

    [preeti]:added UC2 added add contact

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -3,7 +3,9 @@ package com.bridgelabz.addressbook;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to address book system program....");
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact();
 
 	}
 
